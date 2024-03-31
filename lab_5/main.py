75665714db0082e2bc5e385260dee8c4918a630f
Ihor Kolosov <avoqun@gmail.com>
2024-03-31 21:00:06 +0300

    feature/right variant barebones;

--- lab_5/main.py
+++ lab_5/main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 from tkcalendar import DateEntry
 import datetime
+
 class HashTableApp(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -27,7 +28,7 @@ class HashTableApp(tk.Tk):
         date_label = ttk.Label(self, text="Birthdate:")
         date_label.pack(pady=5)
 
-        self.date_picker = DateEntry(self, date_pattern="yyyy-MM-dd")  # Use DateEntry from tkcalendar
+        self.date_picker = DateEntry(self, date_pattern="yyyy-MM-dd")  
         self.date_picker.pack(pady=5)
 
         saved_button = ttk.Button(self, text="Save", command=self.save_student)
@@ -36,20 +37,21 @@ class HashTableApp(tk.Tk):
         clear_button = ttk.Button(self, text="Clear", command=self.clear_student)
         clear_button.pack(pady=5)
 
-        linear_button = ttk.Button(self, text="Linear Probing", command=lambda: self.create_hash_table(self.linear_probing))
-        linear_button.pack(pady=5)
+        division_button = ttk.Button(self, text="Division Hashing", command=lambda: self.create_hash_table(self.hash_division))
+        division_button.pack(pady=5)
 
-        quadratic_button = ttk.Button(self, text="Quadratic Probing", command=lambda: self.create_hash_table(self.quadratic_probing))
-        quadratic_button.pack(pady=5)
+        multiplication_button = ttk.Button(self, text="Multiplication Hashing", command=lambda: self.create_hash_table(self.hash_multiplication))
+        multiplication_button.pack(pady=5)
 
         self.result_text = tk.Text(self, width=40, height=10)
         self.result_text.pack(pady=5)
 
-    def linear_probing(self, index, i, table_size):
-        return (abs(index) + i) % table_size
+    def hash_division(self, key, i, table_size):
+        return (abs(key) + i) % table_size
 
-    def quadratic_probing(self, index, i, table_size):
-        return (abs(index) + i * i) % table_size
+    def hash_multiplication(self, key, i, table_size):
+        constant = 0.6180339887  
+        return int(table_size * ((constant * key) % 1))
 
     def save_student(self):
         name = self.name_entry.get()
